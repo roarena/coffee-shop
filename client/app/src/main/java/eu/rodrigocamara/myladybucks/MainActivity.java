4e153e2de8f5c4a50a3072fa7942424446c46378
Rodrigo Câmara <rodrigoarenacamara@gmail.com>
2017-12-30 03:47:58 -0200

    Loading user information into drawer
    Signed-off-by: Rodrigo Câmara <rodrigoarenacamara@gmail.com>

--- client/app/src/main/java/eu/rodrigocamara/myladybucks/MainActivity.java
+++ client/app/src/main/java/eu/rodrigocamara/myladybucks/MainActivity.java
@@ -2,28 +2,28 @@ package eu.rodrigocamara.myladybucks;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 import eu.rodrigocamara.myladybucks.utils.C;
 import eu.rodrigocamara.myladybucks.utils.Log;
 
@@ -43,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.nvView)
     NavigationView nvDrawer;
 
+    CircleImageView mIvProfilePicture;
+    TextView mTvProfileEmail;
+    TextView mTvProfileName;
+
     private ActionBarDrawerToggle drawerToggle;
 
     @Override
@@ -50,10 +54,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        // Calls for Firebase to initialize user and login if it's a first time user.
-        initializeFirebase();
+
+        /* Workaround due to navigation drawer issue. =(
+        * https://github.com/JakeWharton/butterknife/issues/406
+        * https://code.google.com/p/android/issues/detail?id=190226
+        * */
+        View headerLayout = nvDrawer.inflateHeaderView(R.layout.drawer_header);
+        mTvProfileName = ButterKnife.findById(headerLayout, R.id.tv_profile_name);
+        mTvProfileEmail = ButterKnife.findById(headerLayout, R.id.tv_profile_email);
+        mIvProfilePicture = ButterKnife.findById(headerLayout, R.id.profile_image);
 
         setupDrawerContent(nvDrawer);
+
+        // Calls for Firebase to initialize user and login if it's a first time user.
+        initializeFirebase();
     }
 
     private ActionBarDrawerToggle setupDrawerToggle() {
@@ -87,6 +101,9 @@ public class MainActivity extends AppCompatActivity {
                 mUser = firebaseAuth.getCurrentUser();
                 if (mUser != null) {
                     // Already exists and it's logged.
+                    Picasso.with(getApplicationContext()).load(mUser.getPhotoUrl()).placeholder(R.drawable.profile).into(mIvProfilePicture);
+                    mTvProfileName.setText(mUser.getDisplayName());
+                    mTvProfileEmail.setText(mUser.getEmail());
                     Log.printLog("User already logged. Name: " + mUser.getDisplayName());
                 } else {
                     // First time user.
@@ -111,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == C.LOGIN_ID) {
             if (resultCode == RESULT_OK) {
-                // TODO Call for UI initialization after login process.
+                initializeFirebase();
             } else if (resultCode == RESULT_CANCELED) {
                 finish();
             }
