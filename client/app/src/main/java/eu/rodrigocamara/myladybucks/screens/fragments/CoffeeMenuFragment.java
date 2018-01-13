66421088798ad06bc6df4864ebfb9c0957cf1d08
Rodrigo Câmara <rodrigoarenacamara@gmail.com>
2018-01-13 02:44:51 -0200

    Adding Firebase Realdatabase
    Signed-off-by: Rodrigo Câmara <rodrigoarenacamara@gmail.com>

--- client/app/src/main/java/eu/rodrigocamara/myladybucks/screens/fragments/CoffeeMenuFragment.java
+++ client/app/src/main/java/eu/rodrigocamara/myladybucks/screens/fragments/CoffeeMenuFragment.java
@@ -6,11 +6,19 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,6 +63,52 @@ public class CoffeeMenuFragment extends Fragment {
     }
 
     private void setUIComponents(Context context) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("menu");
+        mCoffeeList = new ArrayList<>();
+        ChildEventListener childEventListener = new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Coffee value = dataSnapshot.getValue(Coffee.class);
+                mCoffeeList.add(value);
+                mCoffeeAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                mCoffeeList.clear();
+
+                Coffee value = dataSnapshot.getValue(Coffee.class);
+                mCoffeeList.add(value);
+                mCoffeeAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                mCoffeeList.clear();
+
+                Coffee value = dataSnapshot.getValue(Coffee.class);
+                mCoffeeList.add(value);
+                mCoffeeAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                mCoffeeList.clear();
+
+                Coffee value = dataSnapshot.getValue(Coffee.class);
+                mCoffeeList.add(value);
+                mCoffeeAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //TODO Treat error properly
+                databaseError.toException();
+            }
+        };
+        myRef.addChildEventListener(childEventListener);
         tvTitle.setText(R.string.menu_title);
 
         if (OrderHelper.getInstance().getOrderList().size() > 0) {
@@ -63,36 +117,12 @@ public class CoffeeMenuFragment extends Fragment {
             btnCart.setText(R.string.go_to_cart_button);
         }
 
-        mCoffeeList = new ArrayList<>();
+
         mCoffeeAdapter = new CoffeeAdapter(context, mCoffeeList);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false);
         rvCoffee.setLayoutManager(mLayoutManager);
         rvCoffee.setItemAnimator(new DefaultItemAnimator());
         rvCoffee.setAdapter(mCoffeeAdapter);
 
-
-        mockMenu();
     }
-
-    private void mockMenu() {
-
-        Coffee coffee = new Coffee("Caffe Latte", "Espresso with light foam and steamed milk", "", 15);
-        mCoffeeList.add(coffee);
-
-        coffee = new Coffee("Cappuccino", "Sweet unique espresso with foamed milk", "", 13);
-        mCoffeeList.add(coffee);
-
-        coffee = new Coffee("Doppio", "Two shots of espresso", "", 13);
-        mCoffeeList.add(coffee);
-
-        coffee = new Coffee("Flat White", "Espresso with light foam and steamed milk", "", 15);
-        mCoffeeList.add(coffee);
-
-        coffee = new Coffee("Moccha", "Espresso combined with foamed milk, layer of chocolate and a really really really really really really really really really really really really really really really really really really really really long string", "", 15);
-        mCoffeeList.add(coffee);
-
-        mCoffeeAdapter.notifyDataSetChanged();
-    }
-
-
 }
